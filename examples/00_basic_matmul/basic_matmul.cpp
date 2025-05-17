66cc66a2592ca929d17c3af440c9f9f283478a60
xutianhao <xutianhao3@h-partners.com>
2025-05-17 16:46:36 +0800

    fix macro definition

--- examples/00_basic_matmul/basic_matmul.cpp
+++ examples/00_basic_matmul/basic_matmul.cpp
@@ -9,6 +9,10 @@
  */
 
 // By setting the K_MAX_SHAPE_DIM macro, the dimension of the AscendC Tensor's ShapeInfo is configured to 0, 
+// optimizing stack space. If you need to use the ShapeInfo of the AscendC Tensor, please undefine this macro.
+#ifndef K_MAX_SHAPE_DIM
+#define K_MAX_SHAPE_DIM 0
+#endif
 
 #include <iostream>
 #include <vector>
