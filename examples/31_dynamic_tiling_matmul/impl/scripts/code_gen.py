351ebccfb040ea4ad2dc4f0f8271e1ac1665726c
祝松祥 <zhusongxiang1@huawei.com>
2025-09-10 00:26:27 +0800

    代码调整

--- examples/31_dynamic_tiling_matmul/impl/scripts/code_gen.py
+++ examples/31_dynamic_tiling_matmul/impl/scripts/code_gen.py
@@ -4,7 +4,7 @@ import itertools
 
 WRAPPER_CODE_PATH = "../warpper"
 
-LAYOUT_TAG_SET = [0, 1] # 0 is RowMajor, 1 is ColumnMajor
+LAYOUT_TAG_SET = [0, 1]  # 0 is RowMajor, 1 is ColumnMajor
 LAYOUT_TAG_MAP = {0: "Catlass::layout::RowMajor", 1: "Catlass::layout::ColumnMajor"}
 
 DTYPE_MAP = {"half": 0, "float": 1}
@@ -36,12 +36,35 @@ size_t {get_workspace_func_name}(TilingParams& tilingParams)
 }}
 """
 
+
 def gen_common_matmul_code(kernel_name, base_file_name, kernel_serial, dtype):
     combinations = list(itertools.product(LAYOUT_TAG_SET, LAYOUT_TAG_SET))
     for l_tag_a, l_tag_b in combinations:
-        launch_kernel_func_name = ("Launch" + kernel_name + dtype.capitalize() + "Layout" + str(l_tag_a) + str(l_tag_b))
-        get_workspace_func_name = (kernel_name + dtype.capitalize() + "Layout" + str(l_tag_a) + str(l_tag_b) + "GetWorkspaceSize")
-        file_name = (base_file_name + "_" + dtype + "_layout" + str(l_tag_a) + str(l_tag_b) + ".cpp")
+        launch_kernel_func_name = (
+            "Launch"
+            + kernel_name
+            + dtype.capitalize()
+            + "Layout"
+            + str(l_tag_a)
+            + str(l_tag_b)
+        )
+        get_workspace_func_name = (
+            kernel_name
+            + dtype.capitalize()
+            + "Layout"
+            + str(l_tag_a)
+            + str(l_tag_b)
+            + "GetWorkspaceSize"
+        )
+        file_name = (
+            base_file_name
+            + "_"
+            + dtype
+            + "_layout"
+            + str(l_tag_a)
+            + str(l_tag_b)
+            + ".cpp"
+        )
 
         element_a = dtype
         element_b = dtype
@@ -50,7 +73,16 @@ def gen_common_matmul_code(kernel_name, base_file_name, kernel_serial, dtype):
         layout_b = LAYOUT_TAG_MAP[l_tag_b]
         layout_c = "Catlass::layout::RowMajor"
 
-        template = common_matmul_template.format(launch_kernel_func_name=launch_kernel_func_name, get_workspace_func_name=get_workspace_func_name, element_a=element_a, element_b=element_b, element_c=element_c, layout_a=layout_a, layout_b=layout_b, layout_c=layout_c)
+        template = common_matmul_template.format(
+            launch_kernel_func_name=launch_kernel_func_name,
+            get_workspace_func_name=get_workspace_func_name,
+            element_a=element_a,
+            element_b=element_b,
+            element_c=element_c,
+            layout_a=layout_a,
+            layout_b=layout_b,
+            layout_c=layout_c,
+        )
 
         with open(os.path.join(WRAPPER_CODE_PATH, file_name), "w") as f:
             f.write(template)
@@ -59,9 +91,4 @@ def gen_common_matmul_code(kernel_name, base_file_name, kernel_serial, dtype):
 if __name__ == "__main__":
     str_dtype = str(sys.argv[1])
 
-    gen_common_matmul_code(
-        "CommonMatmulKernel",
-        "common_matmul_kernel",
-        0,
-        str_dtype
-    )
+    gen_common_matmul_code("CommonMatmulKernel", "common_matmul_kernel", 0, str_dtype)
