7f31dd5c7b46e9cea84af5c930b96e24b41fd289
祝松祥 <zhusongxiang1@huawei.com>
2025-09-10 12:04:21 +0800

    代码调整

--- examples/31_dynamic_tiling_matmul/impl/scripts/code_gen.py
+++ examples/31_dynamic_tiling_matmul/impl/scripts/code_gen.py
@@ -36,18 +36,106 @@ size_t {get_workspace_func_name}(TilingParams& tilingParams)
 }}
 """
 
+launch_map_template = """
+#ifndef LAUNCH_MAP_H
+#define LAUNCH_MAP_H
 
-def gen_common_matmul_code(kernel_name, base_file_name, kernel_serial, dtype):
+#include <unordered_map>
+union TilingKey {{
+    uint64_t value;
+    struct {{
+        uint64_t layoutTagB : 4;  // 0-3
+        uint64_t layoutTagA : 4;  // 4-7
+        uint64_t paddingTagA : 4; // 8-11
+        uint64_t paddingTagB : 4; // 12-15
+        uint64_t paddingTagC : 4; // 16-19
+        uint64_t reserveBit : 32; // 20-51 May be used in the future
+        uint64_t dtype : 4;       // 52-55
+        uint64_t templateKernelSerial : 8; // 56-63
+    }} bits;
+
+    TilingKey() : value(0) {{}}
+    TilingKey(uint64_t v) : value(v) {{}}
+
+    uint8_t GetLayoutTagA() const {{return bits.layoutTagA;}}
+    uint8_t GetLayoutTagB() const {{return bits.layoutTagB;}}
+    uint8_t GetPaddingTagTagA() const {{return bits.paddingTagA;}}
+    uint8_t GetPaddingTagTagB() const {{return bits.paddingTagB;}}
+    uint8_t GetPaddingTagTagC() const {{return bits.paddingTagC;}}
+    uint8_t GetDtype() const {{return bits.dtype;}}
+    uint8_t GetKernelSerial() const {{return bits.templateKernelSerial;}}
+
+    void SetKernelSerial(uint8_t kernelSerial) {{ bits.templateKernelSerial = kernelSerial;}}
+    void SetLayoutTagA(uint8_t layoutTagA) {{ bits.layoutTagA = layoutTagA & 0xF; }}
+    void SetLayoutTagB(uint8_t layoutTagB) {{ bits.layoutTagB = layoutTagB & 0xF; }}
+    void SetPaddingTagA(uint8_t paddingTagA) {{ bits.paddingTagA = paddingTagA & 0xF; }}
+    void SetPaddingTagB(uint8_t paddingTagB) {{ bits.paddingTagB = paddingTagB & 0xF; }}
+    void SetPaddingTagC(uint8_t paddingTagC) {{ bits.paddingTagC = paddingTagC & 0xF; }}
+    void SetDtype(uint8_t dtype) {{ bits.dtype = dtype & 0xF; }}
+
+    void SetTilingKey(uint8_t kernelSerial, uint8_t layoutTagA, uint8_t layoutTagB,
+        uint8_t paddingTagA, uint8_t paddingTagB, uint8_t paddingTagC, uint8_t dtype = 0)
+    {{
+        SetKernelSerial(kernelSerial);
+        SetLayoutTagA(layoutTagA);
+        SetLayoutTagB(layoutTagB);
+        SetPaddingTagA(paddingTagA);
+        SetPaddingTagB(paddingTagB);
+        SetPaddingTagC(paddingTagC);
+        SetDtype(dtype);
+    }}
+}};
+
+#define DECLARE_KERNEL_FUNC(kernelName) \
+    void Launch##kernelName(aclerStream&, uint64_t, uint8_t*, uint8_t*, uint8_t*, uint8_t*, uint8_t*, TilingParams&); \
+    size_t kernelName##GetWorkspaceSize(TilingParams&);
+
+{declare_list}
+
+std::unordered_map<uint64_t, void(*)(aclrtStream&, uint64_t, 
+    uint8_t*, uint8_t*, uint8_t*, uint8_t*, uint8_t*, TilingParams&)> launchKernelFuncMap = {{ 
+{launch_func_list}
+}}
+
+using GetWorkspaceFunc = size_t(*)(TilingParams& tilingParams);
+std::unordered_map<uint64_t, GetWorkspaceFunc> getWorkspaceFuncMap = {{
+{workspace_func_list}
+}}
+
+// only for print kernel Info
+std::unordered_map<uint64_t, std::string> funcNameMap = {{
+{func_name_list}
+}}
+
+
+#endif // LAUNCH_MAP_H
+"""
+
+def get_tiling_key(kernel_serial, dtype, l_tag_a, l_tag_b, p_tag_a, p_tag_b, p_tag_c):
+    part1 = kernel_serial # 56-63
+    part2 = DTYPE_MAP[dtype] << 4 # 48-55
+    part3 = 0 # 40-47
+    part4 = 0 # 32-39
+    part5 = 0 # 24-31
+    part6 = 0 | p_tag_c # 16-23
+    part7 = (p_tag_a << 4) | p_tag_b # 8-15
+    part8 = (l_tag_a << 4) | l_tag_b # 0-7
+    hex_str = f"0x{part1:02x}{part2:02x}{part3:02x}{part4:02x}{part5:02x}{part6:02x}{part7:02x}{part8:02x}"
+    return hex_str
+
+
+def gen_common_matmul_code(kernel_name, base_file_name, kernel_serial, dtype, kernel_info):
     combinations = list(itertools.product(LAYOUT_TAG_SET, LAYOUT_TAG_SET))
     for l_tag_a, l_tag_b in combinations:
-        launch_kernel_func_name = (
-            "Launch"
-            + kernel_name
+        kernel_func_name = (
+            kernel_name
             + dtype.capitalize()
             + "Layout"
             + str(l_tag_a)
             + str(l_tag_b)
         )
+        kernel_info[get_tiling_key(kernel_serial, dtype, l_tag_a, l_tag_b, 0, 0, 0)] = kernel_func_name
+        launch_kernel_func_name = "Launch" + kernel_func_name
         get_workspace_func_name = (
             kernel_name
             + dtype.capitalize()
@@ -87,8 +175,21 @@ def gen_common_matmul_code(kernel_name, base_file_name, kernel_serial, dtype):
         with open(os.path.join(WRAPPER_CODE_PATH, file_name), "w") as f:
             f.write(template)
 
+def gen_launch_map_code(kernel_info):
+    declare_list = "\n".join("DECLARE_KERNEL_FUNC({})".format(value) for value in kernel_info.values())
+    launch_func_list = ",\n".join("{{ {}, Launch{} }}".format(key, value) for key, value in kernel_info.items())
+    workspace_func_list = ",\n".join("{{ {}, {}GetWorkspaceSize }}".format(key, value) for key, value in kernel_info.items())
+    func_name_list = ",\n".join("{{ {}, \"{}\" }}".format(key, value) for key, value in kernel_info.items())
+    template = launch_map_template.format(declare_list=declare_list, launch_func_list=launch_func_list, workspace_func_list=workspace_func_list, func_name_list=func_name_list)
+    with open(os.path.join("../../include", "launch_map.h"), "w") as f:
+            f.write(template)
+
 
 if __name__ == "__main__":
     str_dtype = str(sys.argv[1])
 
-    gen_common_matmul_code("CommonMatmulKernel", "common_matmul_kernel", 0, str_dtype)
+    kernel_info = {}
+
+    gen_common_matmul_code("CommonMatmulKernel", "common_matmul_kernel", 0, str_dtype, kernel_info)
+    gen_launch_map_code(kernel_info)
+    print(kernel_info)
