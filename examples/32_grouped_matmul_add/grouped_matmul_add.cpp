767aba08ec3ff93eb2e5a1c2eb28817a46ec0998
徐天昊 <xutianhao3@h-partners.com>
2025-09-10 10:44:58 +0800

    init

---
+++ examples/32_grouped_matmul_add/grouped_matmul_add.cpp
@@ -0,0 +1,253 @@
+/*
+ * Copyright (c) 2025 Huawei Technologies Co., Ltd.
+ * This file is a part of the CANN Open Software.
+ * Licensed under CANN Open Software License Agreement Version 1.0 (the "License").
+ * Please refer to the License for details. You may not use this file except in compliance with the License.
+ * THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND, EITHER EXPRESS OR IMPLIED,
+ * INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT, MERCHANTABILITY, OR FITNESS FOR A PARTICULAR PURPOSE.
+ * See LICENSE in the root of the software repository for the full text of the License.
+ */
+
+// By setting the K_MAX_SHAPE_DIM macro, the dimension of the AscendC Tensor's ShapeInfo is configured to 0,
+// optimizing stack space. If you need to use the ShapeInfo of the AscendC Tensor, please undefine this macro.
+#ifndef K_MAX_SHAPE_DIM
+#define K_MAX_SHAPE_DIM 0
+#endif
+
+#include <iostream>
+#include <vector>
+
+#include "helper.hpp"
+#include "golden.hpp"
+#include "fp16_t.h"
+
+#include "catlass/catlass.hpp"
+#include "catlass/arch/arch.hpp"
+#include "catlass/epilogue/dispatch_policy.hpp"
+#include "catlass/epilogue/block/block_epilogue.hpp"
+#include "catlass/epilogue/tile/tile_copy.hpp"
+#include "catlass/epilogue/tile/tile_elemwise_add.hpp"
+#include "catlass/gemm/block/block_mmad.hpp"
+#include "catlass/gemm/block/block_swizzle.hpp"
+#include "catlass/gemm/dispatch_policy.hpp"
+#include "catlass/gemm/kernel/grouped_matmul_epilogue.hpp"
+#include "catlass/gemm/gemm_type.hpp"
+#include "catlass/layout/layout.hpp"
+
+#include "catlass/status.hpp"
+#include "catlass/gemm/device/device_gemm.hpp"
+
+using namespace Catlass;
+using fp16_t = op::fp16_t;
+
+struct Options {
+    const std::string HELPER = "24_grouped_matmul_add group_count m n k [device_id]";
+
+    uint32_t groupCount{1};
+    GemmCoord problemShape{128, 128, 128};
+    int32_t deviceId{0};
+
+    Options() = default;
+
+    int Parse(int argc, const char **argv)
+    {
+        enum ArgsIndex {
+            GROUP_COUNT_INDEX = 1,
+            M_INDEX,
+            N_INDEX,
+            K_INDEX,
+            DEVICE_ID_INDEX,
+            ARGS_MAX
+        };
+
+        if (argc > ARGS_MAX || argc <= K_INDEX) {
+            std::cerr << HELPER << std::endl;
+            return -1;
+        }
+
+        groupCount = std::atoi(argv[GROUP_COUNT_INDEX]);
+        problemShape.m() = std::atoi(argv[M_INDEX]);
+        problemShape.n() = std::atoi(argv[N_INDEX]);
+        problemShape.k() = std::atoi(argv[K_INDEX]);
+        if (argc == ARGS_MAX) {
+            deviceId = std::atoi(argv[DEVICE_ID_INDEX]);
+        }
+        return 0;
+    }
+};
+
+void Run(Options const &options)
+{
+    aclrtStream stream{nullptr};
+    ACL_CHECK(aclInit(nullptr));
+    ACL_CHECK(aclrtSetDevice(options.deviceId));
+    ACL_CHECK(aclrtCreateStream(&stream));
+
+    uint32_t problemCount = options.groupCount;
+    uint32_t m = options.problemShape.m();
+    uint32_t n = options.problemShape.n();
+    uint32_t k = options.problemShape.k();
+
+    size_t lenA = static_cast<size_t>(m) * k;
+    size_t lenB = static_cast<size_t>(k) * n;
+    size_t lenD = static_cast<size_t>(m) * n * problemCount;
+
+    size_t sizeA = lenA * sizeof(fp16_t);
+    size_t sizeB = lenB * sizeof(fp16_t);
+    size_t sizeD = lenD * sizeof(fp16_t);
+
+    using LayoutA = layout::RowMajor;
+    using LayoutB = layout::RowMajor;
+    using LayoutC = layout::RowMajor;
+
+    const fp16_t fp16_tLower = -5.0;
+    const fp16_t fp16_tUpper = 5.0;
+    std::vector<fp16_t> hostA(lenA);
+    std::vector<fp16_t> hostB(lenB);
+    std::vector<fp16_t> hostX(lenD);
+    golden::FillRandomData<fp16_t>(hostA, fp16_tLower, fp16_tUpper);
+    golden::FillRandomData<fp16_t>(hostB, fp16_tLower, fp16_tUpper);
+    golden::FillRandomData<fp16_t>(hostX, fp16_tLower, fp16_tUpper);
+
+    auto groupList = golden::GenerateGroupList(k, problemCount);
+    
+    std::vector<GemmCoord> problemShapeList(problemCount);
+    std::vector<LayoutA> layoutAList(problemCount);
+    std::vector<LayoutB> layoutBList(problemCount);
+    std::vector<LayoutC> layoutCList(problemCount);
+    for (uint32_t i = 0; i < problemCount; ++i) {
+        uint32_t currentK = (i == 0) ? groupList[0] : (groupList[i] - groupList[i - 1]);
+        problemShapeList[i] = GemmCoord{m, n, currentK};
+        layoutAList[i] = LayoutA{m, currentK};
+        layoutBList[i] = LayoutB{currentK, n};
+        layoutCList[i] = LayoutC{m, n};
+    }
+
+    uint8_t *deviceA{nullptr};
+    ACL_CHECK(aclrtMalloc(reinterpret_cast<void **>(&deviceA), sizeA, ACL_MEM_MALLOC_HUGE_FIRST));
+    ACL_CHECK(aclrtMemcpy(deviceA, sizeA, hostA.data(), sizeA, ACL_MEMCPY_HOST_TO_DEVICE));
+
+    uint8_t *deviceB{nullptr};
+    ACL_CHECK(aclrtMalloc(reinterpret_cast<void **>(&deviceB), sizeB, ACL_MEM_MALLOC_HUGE_FIRST));
+    ACL_CHECK(aclrtMemcpy(deviceB, sizeB, hostB.data(), sizeB, ACL_MEMCPY_HOST_TO_DEVICE));
+
+    uint8_t *deviceD{nullptr};
+    ACL_CHECK(aclrtMalloc(reinterpret_cast<void **>(&deviceD), sizeD, ACL_MEM_MALLOC_HUGE_FIRST));
+    ACL_CHECK(aclrtMemcpy(deviceD, sizeD, hostX.data(), sizeD, ACL_MEMCPY_HOST_TO_DEVICE));
+
+    uint8_t *problemShapeListDevice{nullptr};
+    size_t sizeProblemShapeList = problemShapeList.size() * sizeof(GemmCoord);
+    ACL_CHECK(aclrtMalloc(reinterpret_cast<void **>(&problemShapeListDevice), sizeProblemShapeList,
+        ACL_MEM_MALLOC_HUGE_FIRST));
+    ACL_CHECK(aclrtMemcpy(problemShapeListDevice, sizeProblemShapeList,
+        problemShapeList.data(), sizeProblemShapeList, ACL_MEMCPY_HOST_TO_DEVICE));
+
+    uint8_t *layoutAListDevice{nullptr};
+    size_t sizeLayoutAList = layoutAList.size() * sizeof(LayoutA);
+    ACL_CHECK(aclrtMalloc(reinterpret_cast<void **>(&layoutAListDevice), sizeLayoutAList, ACL_MEM_MALLOC_HUGE_FIRST));
+    ACL_CHECK(aclrtMemcpy(layoutAListDevice, sizeLayoutAList,
+        layoutAList.data(), sizeLayoutAList, ACL_MEMCPY_HOST_TO_DEVICE));
+
+    uint8_t *layoutBListDevice{nullptr};
+    size_t sizeLayoutBList = layoutBList.size() * sizeof(LayoutB);
+    ACL_CHECK(aclrtMalloc(reinterpret_cast<void **>(&layoutBListDevice), sizeLayoutBList, ACL_MEM_MALLOC_HUGE_FIRST));
+    ACL_CHECK(aclrtMemcpy(layoutBListDevice, sizeLayoutBList,
+        layoutBList.data(), sizeLayoutBList, ACL_MEMCPY_HOST_TO_DEVICE));
+
+    uint8_t *layoutCListDevice{nullptr};
+    size_t sizeLayoutCList = layoutCList.size() * sizeof(LayoutC);
+    ACL_CHECK(aclrtMalloc(reinterpret_cast<void **>(&layoutCListDevice), sizeLayoutCList, ACL_MEM_MALLOC_HUGE_FIRST));
+    ACL_CHECK(aclrtMemcpy(layoutCListDevice, sizeLayoutCList,
+        layoutCList.data(), sizeLayoutCList, ACL_MEMCPY_HOST_TO_DEVICE));
+
+    uint64_t fftsAddr{0};
+    uint32_t fftsLen{0};
+    RT_CHECK(rtGetC2cCtrlAddr(&fftsAddr, &fftsLen));
+
+    auto aicCoreNum = platform_ascendc::PlatformAscendCManager::GetInstance()->GetCoreNumAic();
+
+    using ArchTag = Arch::AtlasA2;
+    constexpr bool enableUnitFlag = true;
+    using MmadDispatchPolicy = Gemm::MmadAtlasA2Pingpong<enableUnitFlag>;
+    using L1TileShape = GemmShape<128, 256, 256>;
+    using L0TileShape = GemmShape<128, 256, 64>;
+    using AType = Gemm::GemmType<half, LayoutA>;
+    using BType = Gemm::GemmType<half, LayoutB>;
+    using CType = Gemm::GemmType<half, LayoutC>;
+    using BlockMmad = Gemm::Block::BlockMmad<MmadDispatchPolicy, L1TileShape, L0TileShape, AType, BType, CType>;
+
+    using EpilogueDispatchPolicy = Epilogue::EpilogueAtlasA2ElemWiseOneSource;
+    using XType = CType;
+    using DType = CType;
+    using ComputeType = CType;
+    constexpr uint32_t computeLength = 16384;
+    using TileElemWiseEpilogue = Epilogue::Tile::TileElemWiseAdd<ArchTag, ComputeType, computeLength>;
+    using EpilogueTileCopy = Epilogue::Tile::TileCopy<ArchTag, CType, XType, DType>;
+    using BlockEpilogue = Epilogue::Block::BlockEpilogue<EpilogueDispatchPolicy, CType, XType, DType,
+        TileElemWiseEpilogue, EpilogueTileCopy>;
+
+    using BlockScheduler = typename Gemm::Block::GemmIdentityBlockSwizzle<3, 1>;
+    using MatmulKernel = Gemm::Kernel::GroupedMatmulEpilogue<BlockMmad, BlockEpilogue, BlockScheduler>;
+    using MatmulAdapter = Gemm::Device::DeviceGemm<MatmulKernel>;
+
+    typename MatmulKernel::Arguments arguments{
+        problemCount,
+        options.problemShape,
+        sizeof(half),
+        problemShapeListDevice,
+        deviceA, layoutAListDevice,
+        deviceB, layoutBListDevice,
+        layoutCListDevice,
+        deviceD};
+
+    MatmulAdapter matmul_op;
+    size_t sizeWorkspace = matmul_op.GetWorkspaceSize(arguments);
+    uint8_t *deviceWorkspace{nullptr};
+    if (sizeWorkspace > 0) {
+        ACL_CHECK(aclrtMalloc(reinterpret_cast<void **>(&deviceWorkspace), sizeWorkspace, ACL_MEM_MALLOC_HUGE_FIRST));
+    }
+    matmul_op.Initialize(arguments, deviceWorkspace);
+    matmul_op(stream, aicCoreNum, fftsAddr);
+    ACL_CHECK(aclrtSynchronizeStream(stream));
+    if (sizeWorkspace > 0) {
+        ACL_CHECK(aclrtFree(deviceWorkspace));
+    }
+
+    // std::vector<fp16_t> hostD(lenD);
+    // ACL_CHECK(aclrtMemcpy(hostD.data(), sizeD, deviceD, sizeD, ACL_MEMCPY_DEVICE_TO_HOST));
+
+    // std::vector<float> hostGolden(lenD);
+    // golden::ComputeGroupedMatmul(problemCount, problemShapeList, hostA, layoutAList,
+    //     hostB, layoutBList, hostGolden, layoutCList);
+    // for (size_t i = 0; i < hostGolden.size(); ++i) {
+    //     hostGolden[i] += static_cast<float>(hostX[i]);
+    // }
+
+    // std::vector<uint64_t> errorIndices = golden::CompareData(hostD, hostGolden, k, groupList, m * n);
+    // if (errorIndices.empty()) {
+    //     std::cout << "Compare success." << std::endl;
+    // } else {
+    //     std::cerr << "Compare failed. Error count: " << errorIndices.size() << std::endl;
+    // }
+
+    ACL_CHECK(aclrtFree(deviceA));
+    ACL_CHECK(aclrtFree(deviceB));
+    ACL_CHECK(aclrtFree(deviceD));
+    ACL_CHECK(aclrtFree(problemShapeListDevice));
+    ACL_CHECK(aclrtFree(layoutAListDevice));
+    ACL_CHECK(aclrtFree(layoutBListDevice));
+    ACL_CHECK(aclrtFree(layoutCListDevice));
+
+    ACL_CHECK(aclrtDestroyStream(stream));
+    ACL_CHECK(aclrtResetDevice(options.deviceId));
+    ACL_CHECK(aclFinalize());
+}
+
+int main(int argc, const char **argv)
+{
+    Options options;
+    if (options.Parse(argc, argv) == 0) {
+        Run(options);
+    }
+    return 0;
+}
---
+++ include/catlass/gemm/kernel/grouped_matmul_epilogue.hpp
@@ -0,0 +1,287 @@
+/*
+ * Copyright (c) 2025 Huawei Technologies Co., Ltd.
+ * This file is a part of the CANN Open Software.
+ * Licensed under CANN Open Software License Agreement Version 1.0 (the "License").
+ * Please refer to the License for details. You may not use this file except in compliance with the License.
+ * THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND, EITHER EXPRESS OR IMPLIED,
+ * INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT, MERCHANTABILITY, OR FITNESS FOR A PARTICULAR PURPOSE.
+ * See LICENSE in the root of the software repository for the full text of the License.
+ */
+
+#ifndef CATLASS_GEMM_KERNEL_GROUPED_MATMUL_EPILOGUE_HPP
+#define CATLASS_GEMM_KERNEL_GROUPED_MATMUL_EPILOGUE_HPP
+
+#include "catlass/catlass.hpp"
+#include "catlass/arch/resource.hpp"
+#include "catlass/arch/cross_core_sync.hpp"
+#include "catlass/coord.hpp"
+#include "catlass/gemm_coord.hpp"
+#include "catlass/matrix_coord.hpp"
+#include "catlass/layout/layout.hpp"
+
+namespace Catlass::Gemm::Kernel {
+
+namespace detail {
+
+template <class T>
+CATLASS_DEVICE void UnpackListParam(T *const dst, GM_ADDR src, uint32_t len)
+{
+    for (uint32_t i = 0; i * sizeof(uint64_t) < len * sizeof(T); ++i) {
+        reinterpret_cast<uint64_t *>(dst)[i] = reinterpret_cast<__gm__ uint64_t *>(src)[i];
+    }
+}
+
+} // namespace detail
+
+// Template for grouped matmul add kernel. Compute grouped D = A * B + X
+// The matmul results are written to a workspace and the epilogue adds X to produce D.
+template <
+    class BlockMmad_,
+    class BlockEpilogue_,
+    class BlockScheduler_
+>
+class GroupedMatmulEpilogue {
+public:
+    using BlockMmad = BlockMmad_;
+    using ArchTag = typename BlockMmad::ArchTag;
+    using L1TileShape = typename BlockMmad::L1TileShape;
+    using ElementA = typename BlockMmad::ElementA;
+    using LayoutA = typename BlockMmad::LayoutA;
+    using ElementB = typename BlockMmad::ElementB;
+    using LayoutB = typename BlockMmad::LayoutB;
+    using ElementC = typename BlockMmad::ElementC;
+    using LayoutC = typename BlockMmad::LayoutC;
+
+    using BlockEpilogue = BlockEpilogue_;
+    using ElementD = typename BlockEpilogue::ElementD;
+    using LayoutD = typename BlockEpilogue::LayoutD;
+
+    using BlockScheduler = BlockScheduler_;
+    static constexpr uint32_t MAX_TENSOR_COUNT = 256;
+
+    struct Params {
+        uint32_t problemCount;
+        GM_ADDR ptrProblemShape;
+        GM_ADDR ptrA;
+        GM_ADDR ptrLayoutA;
+        GM_ADDR ptrB;
+        GM_ADDR ptrLayoutB;
+        GM_ADDR ptrWorkspace;
+        GM_ADDR ptrLayoutC;
+        GM_ADDR ptrD;
+
+        CATLASS_HOST_DEVICE Params() {}
+
+        CATLASS_HOST_DEVICE Params(
+            uint32_t problemCount_, GM_ADDR ptrProblemShape_,
+            GM_ADDR ptrA_, GM_ADDR ptrLayoutA_,
+            GM_ADDR ptrB_, GM_ADDR ptrLayoutB_,
+            GM_ADDR ptrWorkspace_, GM_ADDR ptrLayoutC_,
+            GM_ADDR ptrD_)
+            : problemCount(problemCount_), ptrProblemShape(ptrProblemShape_),
+              ptrA(ptrA_), ptrLayoutA(ptrLayoutA_),
+              ptrB(ptrB_), ptrLayoutB(ptrLayoutB_),
+              ptrWorkspace(ptrWorkspace_), ptrLayoutC(ptrLayoutC_),
+              ptrD(ptrD_) {}
+    };
+
+    struct Arguments {
+        uint32_t problemCount;
+        GemmCoord problemShape; // used for workspace size
+        size_t elementSize;
+        uint8_t *ptrProblemShape;
+        uint8_t *ptrA;
+        uint8_t *ptrLayoutA;
+        uint8_t *ptrB;
+        uint8_t *ptrLayoutB;
+        uint8_t *ptrLayoutC;
+        uint8_t *ptrD; // also stores X
+    };
+
+    static bool CanImplement(const Arguments &args) { return true; }
+
+    static size_t GetWorkspaceSize(const Arguments &args)
+    {
+        return static_cast<size_t>(args.elementSize) *
+               args.problemShape.m() * args.problemShape.n() * args.problemCount;
+    }
+
+    static Params ToUnderlyingArguments(const Arguments &args, uint8_t *workspace)
+    {
+        Params params{
+            args.problemCount, args.ptrProblemShape,
+            args.ptrA, args.ptrLayoutA,
+            args.ptrB, args.ptrLayoutB,
+            workspace, args.ptrLayoutC,
+            args.ptrD};
+        return params;
+    }
+
+    CATLASS_DEVICE GroupedMatmulEpilogue() {}
+
+    template <int32_t CORE_TYPE = g_coreType>
+    CATLASS_DEVICE void operator()(Params const &params);
+
+    template <>
+    CATLASS_DEVICE void operator()<AscendC::AIC>(Params const &params)
+    {
+        GemmCoord problemShapeList[MAX_TENSOR_COUNT];
+        LayoutA layoutAList[MAX_TENSOR_COUNT];
+        LayoutB layoutBList[MAX_TENSOR_COUNT];
+        LayoutC layoutCList[MAX_TENSOR_COUNT];
+
+        detail::UnpackListParam(problemShapeList, params.ptrProblemShape, params.problemCount);
+        detail::UnpackListParam(layoutAList, params.ptrLayoutA, params.problemCount);
+        detail::UnpackListParam(layoutBList, params.ptrLayoutB, params.problemCount);
+        detail::UnpackListParam(layoutCList, params.ptrLayoutC, params.problemCount);
+
+        BlockScheduler matmulBlockScheduler;
+        BlockMmad blockMmad(resource);
+
+        AscendC::GlobalTensor<ElementA> gmA;
+        gmA.SetGlobalBuffer((__gm__ ElementA *)params.ptrA);
+        AscendC::GlobalTensor<ElementB> gmB;
+        gmB.SetGlobalBuffer((__gm__ ElementB *)params.ptrB);
+        AscendC::GlobalTensor<ElementC> gmWorkspace;
+        gmWorkspace.SetGlobalBuffer((__gm__ ElementC *)params.ptrWorkspace);
+
+        uint32_t coreIdx = AscendC::GetBlockIdx();
+        uint32_t coreNum = AscendC::GetBlockNum();
+        int64_t inGroupOffsetA = 0;
+        int64_t inGroupOffsetB = 0;
+        int64_t inGroupOffsetC = 0;
+        uint32_t startCoreIdx = 0;
+
+        for (uint32_t groupIdx = 0; groupIdx < params.problemCount; ++groupIdx) {
+            GemmCoord problemShape = problemShapeList[groupIdx];
+            LayoutA layoutA = layoutAList[groupIdx];
+            LayoutB layoutB = layoutBList[groupIdx];
+            LayoutC layoutC = layoutCList[groupIdx];
+
+            if (problemShape.k() == 0) {
+                inGroupOffsetA += problemShape.m() * problemShape.k();
+                inGroupOffsetB += problemShape.k() * problemShape.n();
+                inGroupOffsetC += problemShape.m() * problemShape.n();
+                continue;
+            }
+
+            matmulBlockScheduler.Update(problemShape, MakeCoord(L1TileShape::M, L1TileShape::N));
+            uint32_t coreLoops = matmulBlockScheduler.GetCoreLoops();
+
+            uint32_t startLoopIdx;
+            if (coreIdx < startCoreIdx) {
+                startLoopIdx = coreIdx + coreNum - startCoreIdx;
+            } else {
+                startLoopIdx = coreIdx - startCoreIdx;
+            }
+
+            for (uint32_t loopIdx = startLoopIdx; loopIdx < coreLoops; loopIdx += coreNum) {
+                GemmCoord blockCoord = matmulBlockScheduler.GetBlockCoord(loopIdx);
+                GemmCoord actualBlockShape = matmulBlockScheduler.GetActualBlockShape(blockCoord);
+
+                MatrixCoord offsetA{blockCoord.m() * L1TileShape::M, blockCoord.k() * L1TileShape::K};
+                MatrixCoord offsetB{blockCoord.k() * L1TileShape::K, blockCoord.n() * L1TileShape::N};
+                MatrixCoord offsetC{blockCoord.m() * L1TileShape::M, blockCoord.n() * L1TileShape::N};
+                int64_t gmOffsetA = layoutA.GetOffset(offsetA);
+                int64_t gmOffsetB = layoutB.GetOffset(offsetB);
+                int64_t gmOffsetC = layoutC.GetOffset(offsetC);
+
+                blockMmad(
+                    gmA[inGroupOffsetA + gmOffsetA], layoutA,
+                    gmB[inGroupOffsetB + gmOffsetB], layoutB,
+                    gmWorkspace[inGroupOffsetC + gmOffsetC], layoutC,
+                    actualBlockShape);
+
+                Arch::CrossCoreSetFlagWithReverse<0x2, PIPE_FIX>(flagAicFinishStore);
+            }
+
+            inGroupOffsetA += problemShape.m() * problemShape.k();
+            inGroupOffsetB += problemShape.k() * problemShape.n();
+            inGroupOffsetC += problemShape.m() * problemShape.n();
+            startCoreIdx = (startCoreIdx + coreLoops) % coreNum;
+        }
+
+        if constexpr (BlockMmad::DispatchPolicy::ASYNC) {
+            blockMmad.SynchronizeBlock();
+        }
+
+        AscendC::PipeBarrier<PIPE_ALL>();
+    }
+
+    template <>
+    CATLASS_DEVICE void operator()<AscendC::AIV>(Params const &params)
+    {
+        GemmCoord problemShapeList[MAX_TENSOR_COUNT];
+        LayoutC layoutCList[MAX_TENSOR_COUNT];
+
+        detail::UnpackListParam(problemShapeList, params.ptrProblemShape, params.problemCount);
+        detail::UnpackListParam(layoutCList, params.ptrLayoutC, params.problemCount);
+
+        BlockScheduler matmulBlockScheduler;
+
+        AscendC::GlobalTensor<ElementC> gmWorkspace;
+        gmWorkspace.SetGlobalBuffer((__gm__ ElementC *)params.ptrWorkspace);
+
+        uint32_t aicoreIndex = AscendC::GetBlockIdx() / AscendC::GetSubBlockNum();
+        uint32_t aicoreNum = AscendC::GetBlockNum();
+
+        int64_t inGroupOffsetC = 0;
+        int64_t inGroupOffsetD = 0;
+        uint32_t startCoreIdx = 0;
+
+        GemmCoord blockShape = L1TileShape::ToCoord();
+        for (uint32_t groupIdx = 0; groupIdx < params.problemCount; ++groupIdx) {
+            GemmCoord problemShape = problemShapeList[groupIdx];
+            LayoutC layoutC = layoutCList[groupIdx];
+            LayoutD layoutD = layoutC; // D shares layout with C
+
+            if (problemShape.k() == 0) {
+                inGroupOffsetC += problemShape.m() * problemShape.n();
+                inGroupOffsetD += problemShape.m() * problemShape.n();
+                continue;
+            }
+
+            typename BlockEpilogue::Params epilogueParams{
+                params.ptrD + sizeof(ElementD) * inGroupOffsetD, layoutD,
+                params.ptrD + sizeof(ElementD) * inGroupOffsetD, layoutD};
+            BlockEpilogue blockEpilogue(resource, epilogueParams);
+
+            matmulBlockScheduler.Update(problemShape, MakeCoord(L1TileShape::M, L1TileShape::N));
+            uint32_t coreLoops = matmulBlockScheduler.GetCoreLoops();
+
+            uint32_t startLoopIdx;
+            if (aicoreIndex < startCoreIdx) {
+                startLoopIdx = aicoreIndex + aicoreNum - startCoreIdx;
+            } else {
+                startLoopIdx = aicoreIndex - startCoreIdx;
+            }
+
+            for (uint32_t loopIdx = startLoopIdx; loopIdx < coreLoops; loopIdx += aicoreNum) {
+                GemmCoord blockCoord = matmulBlockScheduler.GetBlockCoord(loopIdx);
+                GemmCoord actualBlockShape = matmulBlockScheduler.GetActualBlockShape(blockCoord);
+                auto gmBlockC = gmWorkspace[inGroupOffsetC +
+                    layoutC.GetOffset(blockCoord.GetCoordMN() * blockShape.GetCoordMN())];
+                auto layoutBlockC = layoutC.GetTileLayout(actualBlockShape.GetCoordMN());
+
+                Arch::CrossCoreWaitFlagWithReverse<0x2, PIPE_MTE3>(flagAicFinishStore);
+                blockEpilogue(blockShape, blockCoord, actualBlockShape, gmBlockC, layoutBlockC);
+            }
+
+            inGroupOffsetC += problemShape.m() * problemShape.n();
+            inGroupOffsetD += problemShape.m() * problemShape.n();
+            startCoreIdx = (startCoreIdx + coreLoops) % aicoreNum;
+        }
+
+        AscendC::PipeBarrier<PIPE_ALL>();
+    }
+
+private:
+    static constexpr Arch::FlagID FLAG_AIC_FINISH_STORE = 0;
+    static constexpr Arch::FlagID RV_FLAG_AIC_FINISH_STORE = 1;
+    Arch::CrossCoreFlagWithReverse<> flagAicFinishStore{FLAG_AIC_FINISH_STORE, RV_FLAG_AIC_FINISH_STORE};
+    Arch::Resource<ArchTag> resource;
+};
+
+} // namespace Catlass::Gemm::Kernel
+
+#endif // CATLASS_GEMM_KERNEL_GROUPED_MATMUL_EPILOGUE_HPP
