66a4bbda891cf4ebcbd8db35ee1cf65dbe2ec671
祝松祥 <zhusongxiang1@huawei.com>
2025-09-11 22:37:14 +0800

    代码调整

--- examples/31_dynamic_tiling_matmul/impl/scripts/templates/launch_map_template.py
+++ examples/31_dynamic_tiling_matmul/impl/scripts/templates/launch_map_template.py
@@ -1,5 +1,6 @@
 import os
 
+
 class LaunchMapTemplate:
     TEMPLATE = """
 #ifndef LAUNCH_MAP_H
@@ -81,10 +82,25 @@ std::unordered_map<uint64_t, std::string> funcNameMap = {{
 
     @staticmethod
     def gen_code(kernel_info):
-        declare_list = "\n".join("DECLARE_KERNEL_FUNC({})".format(value) for value in kernel_info.values())
-        launch_func_list = ",\n".join("{{ {}, Launch{} }}".format(key, value) for key, value in kernel_info.items())
-        workspace_func_list = ",\n".join("{{ {}, {}GetWorkspaceSize }}".format(key, value) for key, value in kernel_info.items())
-        func_name_list = ",\n".join("{{ {}, \"{}\" }}".format(key, value) for key, value in kernel_info.items())
-        content = LaunchMapTemplate.TEMPLATE.format(declare_list=declare_list, launch_func_list=launch_func_list, workspace_func_list=workspace_func_list, func_name_list=func_name_list)
+        declare_list = "\n".join(
+            "DECLARE_KERNEL_FUNC({})".format(value) for value in kernel_info.values()
+        )
+        launch_func_list = ",\n".join(
+            "{{ {}, Launch{} }}".format(key, value)
+            for key, value in kernel_info.items()
+        )
+        workspace_func_list = ",\n".join(
+            "{{ {}, {}GetWorkspaceSize }}".format(key, value)
+            for key, value in kernel_info.items()
+        )
+        func_name_list = ",\n".join(
+            '{{ {}, "{}" }}'.format(key, value) for key, value in kernel_info.items()
+        )
+        content = LaunchMapTemplate.TEMPLATE.format(
+            declare_list=declare_list,
+            launch_func_list=launch_func_list,
+            workspace_func_list=workspace_func_list,
+            func_name_list=func_name_list,
+        )
         with open(os.path.join("../../include", "launch_map.h"), "w") as f:
-                f.write(content)
+            f.write(content)
