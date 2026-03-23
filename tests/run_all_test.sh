70acb57010fcf1d831d3d134380ac11be9837a78
yuantao_ <yuantao313@outlook.com>
2026-03-23 11:58:10 +0800

    ci适配950
    Co-authored-by: yuantao<taoyuan18@huawei.com>
    
    
    
    # message auto-generated for no-merge-commit merge:
    !513 merge ci_950 into master
    
    ci适配950
    
    Created-by: yuantao_
    Commit-by: yuantao
    Merged-by: cann-robot
    Description: ## 描述
    <!--在这里详细描述你的改动，包括改动的原因和所采取的方法。-->
    1. ci的脚本增加NPU ARCH选项，默认还是2201，兼容性修改
    2. 950目前能运行的分支都运行，遗留aclnn，python extension，gemv conv的独立编译测试
    3. 950硬件可能npu-smi指令有变化，将NPU型号获取函数换成pyacl提供的
    ## 关联的Issue
    <!-- 如果这个PR是为了解决特定的Issue，请在这里提供Issue链接。-->
    
    ## 原因
    <!--说明此次改动的目的、解决的问题等，应与类型标签匹配 -->
    
    ## 测试
    <!--描述进行了哪些测试来验证你的改动。包括但不限于二级冒烟、算子泛化等。-->
    
    ## 文档更新
    <!--如果这个PR包含文档的更新，请在这里指出。例如：更新了README.md文件。-->
    
    ## 类型标签
    <!--  [x] 表示选中 -->
    - [ ] Bug修复
    - [ ] 新特性
    - [ ] 性能优化
    - [ ] 文档更新
    - [ ] 其他，请描述：
    
    See merge request: cann/catlass!513

--- tests/run_all_test.sh
+++ tests/run_all_test.sh
@@ -16,26 +16,34 @@ set -o pipefail
 SCRIPT_PATH=$(dirname "$(realpath "$0")")
 BUILD_SCRIPT_PATH=$(realpath "$SCRIPT_PATH"/../scripts/build.sh)
 
-bash "$SCRIPT_PATH/test_compile.sh"
+if [[ $# -ge 1 ]]; then
+    CATLASS_ARCH="$1"
+else
+    CATLASS_ARCH=2201
+fi
+
+bash "$SCRIPT_PATH/test_compile.sh" "$CATLASS_ARCH"
 
 # example test
 python3 "$SCRIPT_PATH/test_example.py"
 
-# python extension
-bash "$BUILD_SCRIPT_PATH" --clean python_extension || exit 1
-WHEEL_DIR="$SCRIPT_PATH/../output/python_extension/"
-WHEEL_FILE=$(find "$WHEEL_DIR" -type f -name "torch_catlass-*.whl" 2>/dev/null | head -n 1)
-if [ -z "$WHEEL_FILE" ]; then
-    echo "Error: No .whl file found in $WHEEL_DIR"
-    exit 1
+if [[ "$CATLASS_ARCH" == 2201 ]]; then
+    # python extension
+    bash "$BUILD_SCRIPT_PATH" --clean python_extension || exit 1
+    WHEEL_DIR="$SCRIPT_PATH/../output/python_extension/"
+    WHEEL_FILE=$(find "$WHEEL_DIR" -type f -name "torch_catlass-*.whl" 2>/dev/null | head -n 1)
+    if [ -z "$WHEEL_FILE" ]; then
+        echo "Error: No .whl file found in $WHEEL_DIR"
+        exit 1
+    fi
+    pip install "$WHEEL_FILE"
+    python3 "$SCRIPT_PATH/test_python_extension.py"
+    pip uninstall torch_catlass -y
+
+    # torch lib
+    bash "$BUILD_SCRIPT_PATH" --clean torch_library || exit 1
+    python3 "$SCRIPT_PATH/test_torch_lib.py"
+
+    # mstuner_catlass
+    python3 "$SCRIPT_PATH/test_mstuner.py"
 fi
-pip install "$WHEEL_FILE"
-python3 "$SCRIPT_PATH/test_python_extension.py"
-pip uninstall torch_catlass -y
-
-# torch lib
-bash "$BUILD_SCRIPT_PATH" --clean torch_library || exit 1
-python3 "$SCRIPT_PATH/test_torch_lib.py"
-
-# mstuner_catlass
-python3 "$SCRIPT_PATH/test_mstuner.py"
